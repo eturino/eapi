8049b53208903c77a0ecdfab8b7d81b07321d970
Eduardo Turiño <eturino@eturino.com>
2014-07-16 19:34:55 +0100

    finalise in external class

--- lib/eapi/list.rb
+++ lib/eapi/list.rb
@@ -77,6 +77,15 @@ module Eapi
 
     protected :initialize_copy
 
+    # transpose, assoc, rassoc , permutation, combination, repeated_permutation, repeated_combination, product, pack ?? => do not use the methods
+  end
+
+  class ListMethodDefiner
+
+    DESTRUCTIVE_SELF_OR_NIL = [:uniq!, :compact!, :flatten!, :shuffle!, :concat, :clear, :replace, :fill, :reverse!, :rotate!, :sort!, :keep_if]
+
+    DUP_METHODS = [:uniq, :compact, :flatten, :shuffle, :+, :-, :&, :|, :reverse, :rotate, :sort, :split, :in_groups, :in_groups_of, :from, :to]
+
     DELEGATED_METHODS = [
       # normal delegation
       :frozen?, :[], :[]=, :at, :fetch, :first, :last, :<<, :push, :pop, :shift, :unshift, :insert, :length, :size, :empty?, :rindex, :join, :collect, :map, :select, :values_at, :delete, :delete_at, :delete_if, :reject, :include?, :count, :sample, :bsearch, :to_json_without_active_support_encoder, :slice, :slice!, :sort_by!, :shuffle, :shuffle!,
@@ -94,27 +103,43 @@ module Eapi
       :collect!, :map!, :select!, :reject!,
     ]
 
-    delegate *DELEGATED_METHODS, to: :_list
-
-    # to pose as Array
-    alias_method :index, :find_index
-
-    # Destructive methods that return self or nil
-    [:uniq!, :compact!, :flatten!, :shuffle!, :concat, :clear, :replace, :fill, :reverse!, :rotate!, :sort!, :keep_if].each do |m|
-      define_method m do |*args, &block|
-        res = _list.send m, *args, &block
-        res.nil? ? nil : self
-      end
+    def self.finalise(klass)
+      delegate_methods_to_list klass
+      pose_as_array klass
+      destructive_self_or_nil klass
+      dup_methods klass
     end
 
-    # Non destructive methods that return a new object
-    [:uniq, :compact, :flatten, :shuffle, :+, :-, :&, :|, :reverse, :rotate, :sort, :split, :in_groups, :in_groups_of, :from, :to].each do |m|
-      define_method m do |*args, &block|
-        dup.tap { |n| n.initialize_copy(n._list.send m, *args, &block) }
-      end
+    private
+    def self.delegate_methods_to_list(klass)
+      klass.send :delegate, *DELEGATED_METHODS, to: :_list
     end
 
 
-    # transpose, assoc, rassoc , permutation, combination, repeated_permutation, repeated_combination, product, pack ?? => do not use the methods
+    def self.destructive_self_or_nil(klass)
+      # Destructive methods that return self or nil
+      DESTRUCTIVE_SELF_OR_NIL.each do |m|
+        klass.send :define_method, m do |*args, &block|
+          res = _list.send m, *args, &block
+          res.nil? ? nil : self
+        end
+      end
+    end
+
+    def self.pose_as_array(klass)
+      klass.send :alias_method, :index, :find_index
+    end
+
+    def self.dup_methods(klass)
+      # Non destructive methods that return a new object
+      DUP_METHODS.each do |m|
+        klass.send :define_method, m do |*args, &block|
+          dup.tap { |n| n.initialize_copy(n._list.send m, *args, &block) }
+        end
+      end
+    end
+
   end
+
+  ListMethodDefiner.finalise List
 end
